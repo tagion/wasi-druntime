bdd4660343b7ad2e11b46126dd0c4423cd6975b5
carsten <cr@tagion.org>
2020-08-29 10:50:48 +0300

    WIP: wasi_libc.mk added

---
+++ wasi_libc.mk
@@ -0,0 +1,9 @@
+
+WASI_BIN:=$(REPOROOT)/wasi-sdk-8.0/bin
+
+WASM_AR:=$(WASI_BIN)/llvm-ar
+WASM_NM:=$(WASI_BIN)/llvm-nm
+WASM_CC:=$(WASI_BIN)/clang
+
+build-wasi-libc:
+	make -C wasi-libc WASM_CC=$(WASM_CC) WASM_AR=$(WASM_AR) WASM_NM=$(WASM_NM)
