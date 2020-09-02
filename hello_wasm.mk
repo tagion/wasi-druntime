582ef03fec417a0d47b0391a72a263e52c49e375
carsten <cr@tagion.org>
2020-09-02 14:56:02 +0300

    The test program can now build without updating the /etc/ldc2.conf

--- hello_wasm.mk
+++ hello_wasm.mk
@@ -6,6 +6,10 @@ MAIN:=hello_wasm.wasm
 DFILES+=hello_wasm.d
 OBJS:=${DFILES:.d=.o}
 
+DCFLAGS+=-defaultlib=c,druntime-ldc,phobos2-ldc
+DCFLAGS+=-I$(LDC_RUNTIME_ROOT)/druntime/src
+DCFLAGS+=-I$(LDC_RUNTIME_ROOT)/phobos
+
 DCFLAGS+=-mtriple=wasm32-unknown-unknown-wasm
 DCFLAGS+=-c
 
