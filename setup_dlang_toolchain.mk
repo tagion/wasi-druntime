2606ad470fb9df95cb7f6308954fe24b6485cf45
cbleser <cr@tagion.org>
2026-01-21 13:16:12 +0200

    wip(wasi-sdk): updated to version 29

--- setup_dlang_toolchain.mk
+++ setup_dlang_toolchain.mk
@@ -8,13 +8,10 @@ LDC_NAME:=ldc-$(LDC_VERSION)
 DLANG_PATH=$(HOME)/dlang/$(LDC_NAME)
 LDC_BIN:=$(DLANG_PATH)/bin
 LDC_WASI:=$(LDC_BIN)/ldc2
-LDC_SOURCE:=$(DLANG_PATH)/activate
-DC?=$(LDC_BIN)/ldc2
-LDC_CONF?=$(DLANG_PATH)/etc/ldc2.conf
 
 $(DLANG_INSTALL):
 	$(PRECMD)
-	mkdir -p $(DLANG)
+	$(MKDIR) -p $(DLANG)
 	wget $(DLANG_INSTALL_URL) -O $@
 	chmod 750 $@
 
@@ -25,21 +22,66 @@ $(DLANG_PATH): $(DLANG_INSTALL)
 
 install-dlang: $(DLANG_PATH)
 
-.PHONY: install-dlang
+ifdef DONT
+LDC_HOST:=ldc2-${LDC_VERSION}-linux-x86_64
+LDC_HOST_TAR:=$(LDC_HOST).tar.xz
+LDC_WASI_BIN:=$(TOOLS)/$(LDC_HOST)/bin
+LDC_WASI:=$(LDC_WASI_BIN)/ldc2
+LDC_URL:=https://github.com/ldc-developers/ldc/releases/download/v${LDC_VERSION}
+LDC_URL_TAR:=$(LDC_URL)/${LDC_HOST_TAR}
 
-env-dlang:
-	@echo "-----  $@ :: env"
-	@echo "DLANG_PATH        = $(DLANG_PATH)"
-	@echo "DLANG_INSTALL     = $(DLANG_INSTALL)"
-	@echo "LDC_BIN           = $(LDC_BIN)"
-	@echo "LDC_SOURCE        = $(LDC_SOURCE)"
-	@echo "DC                = $(DC)"
-	@echo "LDC_CONF          = $(LDC_CONF)"
-	@echo
+export DC=$(LDC_WASI)
 
-.PHONY: env-dlang
+install-wasi-wasm-toolchain: $(LDC_HOST) 
+.PHONY: install-wasi-wasm-toolchain
 
-env: env-dlang
+$(TOOLS)/.way:
+	mkdir -p $(TOOLS)
+	touch $(TOOLS)/.way
 
-uninstall-dlang:
-	rm -fR $(DLANG_PATH)
+$(TOOLS)/$(LDC_HOST)/etc/ldc2.conf: $(TOOLS)/$(LDC_HOST)/.done tub/ldc2.conf
+	mv $(TOOLS)/$(LDC_HOST)/etc/ldc2.conf $(TOOLS)/$(LDC_HOST)/etc/ldc2.conf.orig || true
+	cp tub/ldc2.conf $(TOOLS)/$(LDC_HOST)/etc/ldc2.conf
+
+$(LDC_HOST): $(TOOLS)/$(LDC_HOST)/.done
+$(LDC_HOST): $(TOOLS)/$(LDC_HOST)/etc/ldc2.conf
+.PHONY: $(LDC_HOST)
+
+$(TOOLS)/$(LDC_HOST)/.done: $(TOOLS)/.way
+	cd $(TOOLS)
+	wget ${LDC_URL_TAR} -O ${LDC_HOST_TAR}
+	tar xf $(LDC_HOST_TAR)
+	touch $@
+
+clean-tools:
+	$(RM) -vr $(TOOLS)
+
+.PHONY: clean-tools
+
+env-install-wasi:
+	$(PRECMD)
+	$(call log.header, $@ :: env)
+	$(call log.kvp, LDC_VERSION, $(LDC_VERSION)) 
+	$(call log.kvp, LDC_URL_TAR, $(LDC_URL_TAR))
+	$(call log.kvp, LDC_WASI_BIN, $(LDC_WASI_BIN))
+	$(call log.kvp, LDC_WASI, $(LDC_WASI))
+	$(call log.close)
+
+.PHONY: env-install-wasi
+
+env: env-install-wasi
+
+help-install-wasi:
+	$(PRECMD)
+	$(call log.header, $@ :: help)
+	$(call log.help, "make help-install-wasi", "Will show this help text")
+	$(call log.help, "make install-wasi-wasm-toolchain", "Install the LDC2 compiler and wasi library")
+	$(call log.help, "make clean-tools", "Remove the tool chain")
+	$(call log.close)
+
+.PHONY: help-install-wasi
+
+help: help-install-wasi
+
+proper: clean-tools
+endif
